04d946e96e144f92d107d9d26a30f3bddde9a625
fcovalenzuela <fvalenza@gmail.com>
2014-07-10 23:23:55 -0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -13,5 +13,5 @@ data<-subset(household, Date== as.Date("2007-02-01") | Date==as.Date("2007-02-02
 par(mfrow=c(1,1))
 plot02<-plot(data$y,as.character(data$Global_active_power), col="black", type="l", xlab="",
              ylab = "Global Active Power (Kilowatts)")
-dev.copy(png, file = "plot2.png",width=480, height=480, res=360)
+dev.copy(png, file = "plot2.png")
 dev.off() 
